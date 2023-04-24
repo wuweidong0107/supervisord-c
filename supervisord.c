b45dadd000d6f0a7c022d4d6063bdd6971697b20
JackWu <625769020@qq.com>
2023-04-24 23:55:04 +0800

    able to run program

--- supervisord.c
+++ supervisord.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <glob.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 #include "ini.h"
 #include "stdstring.h"
@@ -11,15 +15,95 @@
 typedef struct program {
     char *name;
     char *command;
+    bool is_autostart;
+    bool is_autorestart;
+    bool is_running;
+    pid_t pid;
     struct list_head list;
 } program_t;
 
 static LIST_HEAD(program_list);
+static char *conf_file = "/home/wwd/all/libwwd/src/tools/c/0025_c_supervisord/conf.d/*.conf";
+static int exiting = 0;
+static int got_sigchld = 0;
+
+static void handle_signal(int sig)
+{
+    switch (sig) {
+        case SIGINT:
+        case SIGTERM:
+            exiting = 1;
+            break;
+        case SIGCHLD:
+            got_sigchld = 1;
+            break;
+    }
+}
+
+static int start_program(program_t *prog)
+{
+    char *args[1] = {NULL};
+
+    if (prog->is_running)
+        return 0;
+    
+    int child_pid;
+    switch (child_pid = fork()) {
+        case -1: 
+            return -1;
+        case 0:
+            // TBD
+            execv(prog->command, args);
+            exit(1);
+        default:
+            prog->is_running = true;
+            prog->pid = child_pid;
+            printf("[Start] program: %s %s pid=%d\n", prog->name, prog->command, prog->pid);
+            break;
+    }
+    return 0;
+}
+
+static program_t* get_program(pid_t pid)
+{
+    program_t *prog;
+    list_for_each_entry(prog, &program_list, list) {
+        if (prog->pid == pid) {
+            return prog;
+        }
+    }
+    return NULL;
+}
+
+static void handle_sigchld()
+{
+    program_t *prog;
+    int killed_pid;
+
+    do {
+        killed_pid = waitpid(-1, 0, WNOHANG);
+        if (killed_pid > 0) {
+            prog = get_program(killed_pid);
+
+            if (prog) {
+                prog->is_running = 0;
+                prog->pid = 0;
+                printf("[Exited] program: %s pid=%d\n", prog->name, killed_pid);
+
+                if (prog->is_autorestart) {
+                    start_program(prog);
+                }
+            }
+        }
+    } while (killed_pid > 0);
+}
 
 static program_t* parse_conf(char* conf_file)
 {
     char section[256] = {0};
-    int s;
+    char key[256] = {0};
+    char command[256] = {0};
+    int s, k;
 
     for (s = 0; ini_getsection(s, section, sizearray(section), conf_file) > 0; s++) {
         if (string_starts_with(section, "program:")) {
@@ -33,7 +117,17 @@ static program_t* parse_conf(char* conf_file)
                     fprintf(stderr, "Unable to allocate memory for program!\n");
                     return NULL;
                 }
+                for (k = 0; ini_getkey(section, k, key, sizearray(key), conf_file) > 0; k++) {
+                    if (!strncmp(key, "command", strlen("command"))) {
+                        ini_gets(section, key, "dummy", command, sizearray(command), conf_file);
+                        prog->command = strdup(command);
+                    }
+                } 
                 prog->name = strdup(tokens[1]);
+                prog->is_autostart = true;
+                prog->is_autorestart = false;
+                prog->is_running = false;
+
                 return prog;
             }
             for(int j=0; j<count; j++)
@@ -49,6 +143,10 @@ static void clear_programs()
     program_t *prog;
     while(!list_empty(&program_list)) {
         prog = list_first_entry(&program_list, program_t, list);
+        if (prog->is_running) {
+            printf("Kill child process: %s pid: %d\n", prog->name, prog->pid);
+            kill(prog->pid, SIGKILL);
+        }
         if (prog->name) free(prog->name);
         if (prog->command) free(prog->command);
         list_del(&prog->list);
@@ -61,13 +159,13 @@ static void append_program(program_t *prog)
     list_add_tail(&prog->list, &program_list);
 }
 
-int main()
+static int reload_conf(void)
 {
-    char *conf="/home/wwd/all/libwwd/src/tools/c/0025_c_supervisord/conf.d/daemon-configs/conf.d/*.conf";
     glob_t globres;
     int i;
 
-    if (glob(conf, 0, NULL, &globres))
+    clear_programs();
+    if (glob(conf_file, 0, NULL, &globres))
         return -1;
     for(i = 0; i < globres.gl_pathc; i++) {
         printf("%s\n", globres.gl_pathv[i]);
@@ -77,6 +175,48 @@ int main()
         }
     }
     globfree(&globres);
+    return 0;
+}
+
+static void cleanup()
+{
     clear_programs();
+}
+
+int main()
+{
+    /* load config file */
+    if (reload_conf() != 0) {
+        return 1;
+    }
+
+    /* install signal handler */
+    struct sigaction handler;
+    handler.sa_handler = handle_signal;
+    sigfillset(&handler.sa_mask);
+    handler.sa_flags=0;
+    sigaction(SIGINT,&handler,0);
+    sigaction(SIGTERM,&handler,0);
+    sigaction(SIGHUP,&handler,0);
+    handler.sa_flags = SA_RESTART|SA_NOCLDSTOP;
+    sigaction(SIGCHLD,&handler,0);
+
+    program_t *prog;
+    list_for_each_entry(prog, &program_list, list) {
+        if (prog->is_autostart) {
+            start_program(prog);
+        }
+    }
+
+    while (!exiting) {
+        sleep(1);
+        if (got_sigchld) {
+            handle_sigchld();
+            got_sigchld = 0;
+        }
+    }
+
+    printf("cleanup...\n");
+    cleanup();
     return 0;
 }
